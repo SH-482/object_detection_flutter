f681af015a7a9e277436251c4d7d4139604f91ee
SH-482 <shsharjeel482@gmail.com>
2023-10-22 20:26:51 +0500

    throwing error about the manifest not found

--- lib/controller/scan_controller.dart
+++ lib/controller/scan_controller.dart
@@ -1,5 +1,8 @@
 
+import 'dart:math';
+
 import 'package:camera/camera.dart';
+import 'package:flutter_tflite/flutter_tflite.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -9,6 +12,7 @@ class ScanController extends GetxController{
 
     super.onInit();
      initCamera();
+     initTFLite();
   }
   @override
   void dispose() {
@@ -18,6 +22,7 @@ class ScanController extends GetxController{
   late CameraController cameraController;
   late List<CameraDescription> cameras;
   var isCameraInitialized = false.obs;
+  var cameraCount = 0;
 
   initCamera()async{
     if(await Permission.camera.request().isGranted){
@@ -26,13 +31,43 @@ class ScanController extends GetxController{
         cameras[0],
         ResolutionPreset.max
       );
-      await cameraController.initialize();
+      await cameraController.initialize().then((value){
+
+          cameraController.startImageStream((image) {
+            cameraCount++;
+            if(cameraCount %10==0){
+              cameraCount = 0;
+              objectDetector(image);
+            }
+            update();
+            } );
+
+
+      });
       isCameraInitialized(true);
       update();
   }else{
       print("Permission Denied");
     }
     }
+    initTFLite() async{
+    await Tflite.loadModel(model: "assets/model.tflite",
+    labels: "assets/label.txt",
+    isAsset: true,numThreads: 1,useGpuDelegate: false,);
+    }
 
-
+    objectDetector (CameraImage image) async{
+    var detector = Tflite.runModelOnFrame(bytesList: image.planes.map((e){return e.bytes;}).toList(),asynch: true,
+    imageHeight: image.height,
+    imageWidth: image.width,
+    imageMean: 127.5,
+    imageStd: 127.5,
+    numResults: 1,
+    rotation: 90,
+    threshold: 0.4,
+    );
+    if(detector !=null){
+      log("Result is $detector" as num);
+    }
+    }
 }
