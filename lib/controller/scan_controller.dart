6df9802589674c758053c290003281c0c0f60efb
SH-482 <shsharjeel482@gmail.com>
2023-10-23 11:51:52 +0500

    error resolved but application is crashing

--- lib/controller/scan_controller.dart
+++ lib/controller/scan_controller.dart
@@ -1,4 +1,3 @@
-
 import 'dart:math';
 
 import 'package:camera/camera.dart';
@@ -6,68 +5,73 @@ import 'package:flutter_tflite/flutter_tflite.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-class ScanController extends GetxController{
+class ScanController extends GetxController {
   @override
   void onInit() {
-
     super.onInit();
-     initCamera();
-     initTFLite();
+    initCamera();
+    initTFLite();
   }
+
   @override
   void dispose() {
     super.dispose();
     cameraController.dispose();
   }
+
   late CameraController cameraController;
   late List<CameraDescription> cameras;
   var isCameraInitialized = false.obs;
   var cameraCount = 0;
 
-  initCamera()async{
-    if(await Permission.camera.request().isGranted){
+  initCamera() async {
+    if (await Permission.camera.request().isGranted) {
       cameras = await availableCameras();
-      cameraController = await CameraController(
-        cameras[0],
-        ResolutionPreset.max
-      );
-      await cameraController.initialize().then((value){
-
-          cameraController.startImageStream((image) {
-            cameraCount++;
-            if(cameraCount %10==0){
-              cameraCount = 0;
-              objectDetector(image);
-            }
-            update();
-            } );
-
-
+      cameraController =
+          await CameraController(cameras[0], ResolutionPreset.max);
+      await cameraController.initialize().then((value) {
+        cameraController.startImageStream((image) {
+          cameraCount++;
+          if (cameraCount % 10 == 0) {
+            cameraCount = 0;
+            objectDetector(image);
+          }
+          update();
+        });
       });
       isCameraInitialized(true);
       update();
-  }else{
+    } else {
       print("Permission Denied");
     }
-    }
-    initTFLite() async{
-    await Tflite.loadModel(model: "assets/model.tflite",
-    labels: "assets/label.txt",
-    isAsset: true,numThreads: 1,useGpuDelegate: false,);
-    }
+  }
 
-    objectDetector (CameraImage image) async{
-    var detector = Tflite.runModelOnFrame(bytesList: image.planes.map((e){return e.bytes;}).toList(),asynch: true,
-    imageHeight: image.height,
-    imageWidth: image.width,
-    imageMean: 127.5,
-    imageStd: 127.5,
-    numResults: 1,
-    rotation: 90,
-    threshold: 0.4,
+  initTFLite() async {
+    await Tflite.loadModel(
+      model: "assets/ssd_mobilenet.tflite",
+      labels: "assets/ssd_mobilenet.txt",
+      isAsset: true,
+      numThreads: 1,
+      useGpuDelegate: false,
     );
-    if(detector !=null){
+  }
+
+  objectDetector(CameraImage image) async {
+    var detector = Tflite.runModelOnFrame(
+      bytesList: image.planes.map((e) {
+        return e.bytes;
+      }).toList(),
+      asynch: true,
+      imageHeight: image.height,
+      imageWidth: image.width,
+      imageMean: 127.5,
+      imageStd: 127.5,
+      numResults: 1,
+      rotation: 90,
+      threshold: 0.4,
+    );
+    if (detector != null) {
       log("Result is $detector" as num);
     }
-    }
+  }
 }
